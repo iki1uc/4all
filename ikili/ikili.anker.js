caef9259ff7056d298526e28b0b84479f0622dbc
iki1uc <entertrainer@web.de>
2026-06-07 17:34:16 +0200

    Update ikili.anker.js

--- ikili/ikili.anker.js
+++ ikili/ikili.anker.js
@@ -1,1 +1,24 @@
+// ikili.anker.js
+// 4ALL Anker-System (einzigartig)
+
+const ikiliAnker = {
+    id: "ikili-anker",
+    type: "4ALL",
+    active: false,
+
+    init() {
+        this.active = true;
+        console.log("ikili.anker → aktiv");
+    },
+
+    info() {
+        return {
+            name: "ikili",
+            typ: "4ALL-Gruppierungsanker",
+            status: this.active
+        };
+    }
+};
+
+export default ikiliAnker;
 
