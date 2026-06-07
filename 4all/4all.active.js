30c6496fd11cfd9e9c39f140d3c9a1011ebb08d3
iki1uc <entertrainer@web.de>
2026-06-07 18:41:30 +0200

    Update 4all.active.js

--- 4all/4all.active.js
+++ 4all/4all.active.js
@@ -1,1 +1,18 @@
+import { ZeroThreeZero } from "./z3.zero.js";
+import { FourAll2be4 } from "./4all.2be4.js";
+
+export const FourAllActive = {
+    ready: false,
+
+    start() {
+        this.ready = true;
+
+        console.log("4ALL aktiv – bereit für Arbeit");
+        console.log("ZeroThree 0/1:", ZeroThreeZero.getZero(), ZeroThreeZero.getOne());
+
+        FourAll2be4.update();
+    }
+};
+
+FourAllActive.start();
 
